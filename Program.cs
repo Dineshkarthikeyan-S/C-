65fc741e9e8175005990a6b3122910c394f4e777
Dineshkarthikeyan-S <dineshkarthikeyan2611@gmail.com>
2024-11-06 10:24:24 +0530

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConAppThreadingEx;
+
+Console.WriteLine("Hello, World!");
+Thread thread = new Thread(new ThreadStart(OurClass.Display));
+
+thread.Start();
+
